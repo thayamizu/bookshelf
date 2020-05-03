a49c836713fa10fddd348d018c1ccf89032b2dd4
tatsuya hayamizu <t.hayamizu@outlook.com>
2020-05-03 23:20:24 +0900

    read 07-02

---
+++ getting-start-with-os-in-30-days/07_day/harib04b/bootpack.c
@@ -0,0 +1,48 @@
+#include "bootpack.h"
+#include "descriptors.h"
+#include "graphic.h"
+#include "my_sprintf.h"
+#include "pic.h"
+
+void HariMain(void)
+{
+
+    BootInfo *binfo = (BootInfo *)0x00000ff0;
+    char s[40], mcursor[256];
+    int mx, my;
+
+    init_gdtidt();
+    init_pic();
+    io_sti();
+
+    init_palette();
+    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+    mx = (binfo->scrnx - 16) / 2;
+    my = (binfo->scrny - 28 - 16) / 2;
+    init_mouse_cursor8(mcursor, COL8_008484);
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+    my_sprintf(s, "(%d, %d)", mx, my);
+    putfont8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+
+    io_out8(PIC0_IMR, 0xf9);
+    io_out8(PIC1_IMR, 0xef);
+
+    for (;;)
+    {
+        io_cli();
+        if (keybuf.flag == 0)
+        {
+            io_stihlt();
+        }
+        else
+        {
+            unsigned char i = keybuf.data;
+            keybuf.flag = 0;
+
+            my_sprintf(s, "%x", i);
+            boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+            putfont8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+        }
+        io_hlt();
+    }
+}
---
+++ getting-start-with-os-in-30-days/07_day/harib04b/bootpack.h
@@ -0,0 +1,56 @@
+#ifndef MY_BOOTPACK_H_
+#define MY_BOOTPACK_H_
+
+/**
+ * BootInfo構造体
+ */
+typedef struct _BootInfo
+{
+    char cyls, leds, vmode, reserve;
+    short scrnx, scrny;
+    char *vram;
+} BootInfo;
+
+/**
+ * ハルトします
+ * @return void
+ */
+extern void io_hlt(void);
+
+extern void io_sti(void);
+/**
+ * CLI 
+ * @return void
+ */
+extern void io_cli(void);
+
+/**
+ * STIHLT
+ *  @return void
+ */
+extern void io_stihlt(void);
+
+/**
+ * 
+ * @return void
+ */
+extern void io_out8(int port, int data);
+
+/**
+ * eflagsを取得します
+ * @return int
+ */
+extern int io_load_eflags(void);
+
+/**
+ * eflagsを保存します
+ * @return void
+ */
+extern void io_store_eflags(int eflags);
+
+/**
+ * 
+ * @return int
+ */
+extern int io_in8(char port);
+#endif
